07b797124510b5affa421cff627230076a467996
Rasa94 <radivojejovanovic94@gmail.com>
2021-02-07 13:13:29 +0100

    Trying to implement the user model

---
+++ server/models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an email'],
+        unique: true,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email',
+        ],
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
+        minlength: 6,
+        select: false
+    }
+})
